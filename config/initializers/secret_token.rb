793a5f7e21bdce8bf236873806cd6ccfd2fa5e56
Liz Abinante <eabinante@gmail.com>
2013-07-30 12:57:52 -0500

    removed rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Upesteem::Application.config.secret_token = 'd650e3a87d49c8a70af9de79b6125b554346c5eedf614ecbc7176d1ccb4dd47699cf8ed05e357613421ac2d74e1956cfcca1187f90f55ffe5bbb095688b482e5'
